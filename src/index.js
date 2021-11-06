b2085a307cbec637640005bf77d39c0dedcf6776
keensyo <k66mdg55@gmail.com>
2021-11-06 23:46:33 +0900

    map, filter

--- src/index.js
+++ src/index.js
@@ -136,12 +136,12 @@
 // console.log(arr3);
 
 // 配列のコピー、結合
-const arr4 = [10, 20];
-const art5 = [30, 40];
+// const arr4 = [10, 20];
+// const art5 = [30, 40];
 
-const arr6 = [...arr4];
-arr6[0] = 100;
-console.log(arr4);
+// const arr6 = [...arr4];
+// arr6[0] = 100;
+// console.log(arr4);
 
 // const arr7 = [...arr4, ...art5];
 // console.log(arr7);
@@ -149,3 +149,35 @@ console.log(arr4);
 // const arr8 = arr4;
 // arr8[0] = 100;
 // console.log(arr4);
+
+/**
+ * mapやfilterを使った配列の処理
+ */
+const nameArr = ["aaa", "bbb", "ccc"];
+// for (let index = 0; index < nameArr.length; index++) {
+//   console.log(nameArr[index]);
+// }
+
+// const nameArr2 = nameArr.map((name) => {
+//   return name;
+// })
+// console.log(nameArr2);
+
+// nameArr.map((name) => console.log(name));
+
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 1;
+// });
+// console.log(newNumArr);
+
+// nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です`));
+
+const newNameArr = nameArr.map((name) => {
+  if (name === "aaa") {
+    return name;
+  } else {
+    return `${name}さん`;
+  }
+});
+console.log(newNameArr);
